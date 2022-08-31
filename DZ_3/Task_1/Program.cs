5a64e348738057a01c346512efbccc75e8725e7b
Aleksey <pilot-1981@yandex.ru>
2022-08-31 16:50:45 +0500

    add DZ_3-Task_1

--- DZ_3/Task_1/Program.cs
+++ DZ_3/Task_1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write ("Дано пятизначное число: ");
+// int num = 12321; // проверка!
+int num = new Random().Next(10000, 99999);
+Console.WriteLine (num);
+int num1 = (num /10000) % 10; //1 число
+int num2 = (num /1000) % 10; //2 число
+int num4 = (num / 10) % 10; // 4 число
+int num5 = num % 10; // 5 число
+
+if ((num1 == num5) && (num2 == num4))
+{
+    Console.WriteLine ("Число является палиндромом!");
+}
+else
+{
+    Console.WriteLine ("Число не является палиндромом!");
+}
+
