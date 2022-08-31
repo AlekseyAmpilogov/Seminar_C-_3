5e6281a7ea86826b6222e37a5b8c76a5e370a1e6
Aleksey <pilot-1981@yandex.ru>
2022-08-31 11:59:05 +0500

    add Sem_3-Task_1

--- Sem_3/Task_1/Program.cs
+++ Sem_3/Task_1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine ("Введите координату x: ");
+int x = int.Parse (Console.ReadLine());
+Console.WriteLine ("Введите координату y: ");
+int y = int.Parse (Console.ReadLine());
+
+if ((x != 0) && (y != 0))
+{
+    if ((x > 0) && (y > 0))
+    {
+        Console.WriteLine("1 четверть плоскости координат!");
+    }
+    else if ((x > 0) && (y < 0))
+    {
+        Console.WriteLine("2 четверть плоскости координат!");
+    }
+    else if ((x < 0) && (y > 0))
+    {
+        Console.WriteLine("3 четверть плоскости координат!");
+    }
+    else if ((x < 0) && (y < 0))
+    {
+        Console.WriteLine("4 четверть плоскости координат!");
+    }
+}
