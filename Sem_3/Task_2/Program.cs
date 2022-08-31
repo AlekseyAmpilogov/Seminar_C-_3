daedfb13861632b5e7f71ddbd6fd03f184eebd0c
Aleksey <pilot-1981@yandex.ru>
2022-08-31 12:09:50 +0500

    add Sem_3-Task_2

---
+++ Sem_3/Task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите количество элементов в массиве: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [N];
+
+int index = 0;
+while (index < N)
+{
+    array [index] = new Random().Next(1, 11); // заполнение элементов массива от 1 до 10
+    Console.Write (array [index] + " "); // выводит элементы массива на экран
+    index++;
+}
+int sum = 0; // создаем переменную и присваиваем ей значение 0
+for (int i=0; i < N; i++)
+{
+    sum += array [i]; // суммирование с накоплением
+}
+double mean = (double)sum / N;
+Console.WriteLine();
+Console.WriteLine("Среднеарифметическое значение данного массива: " + mean);
