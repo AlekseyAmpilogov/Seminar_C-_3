5fa76572b197f42d442967bd34e52828dff46739
Aleksey <pilot-1981@yandex.ru>
2022-08-31 17:34:46 +0500

    add DZ_3-Task_2

---
+++ DZ_3/Task_2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine ("Введите координаты первой точки: ");
+Console.Write ("x1 = ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write ("y1 = ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write ("z1 = ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите координаты второй точки: ");
+Console.Write ("x2 = ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write ("y2 = ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write ("z2 = ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+Double distance;
+
+{
+    distance = Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)+Math.Pow(z2-z1,2)); // Находим расстояния между точками по формуле 
+    Console.Write ("Расстояние между точками = " + distance);
+}
