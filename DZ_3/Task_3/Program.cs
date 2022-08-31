a2c9c455ba1b94fac5c827d290dccedf9fdb3e25
Aleksey <pilot-1981@yandex.ru>
2022-08-31 17:41:36 +0500

    add DZ_3-Task_3

---
+++ DZ_3/Task_3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int [] array = new int [N];
+
+int i = 0;
+int num = 1;
+Console.Write ("Получен массив квадратных чисел от 1 до " + N*N + " : ");
+while (i < N)
+{
+    array [i] = num * num * num;
+    Console.Write (array [i] + " ");
+    num ++;
+    i ++;
+}
